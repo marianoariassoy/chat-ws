32c60ac372bb20393b86f70c7858522057ae11ca
marianoariassoy <hola@marianoarias.soy>
2023-08-11 11:40:49 -0300

    add tailwind css

--- public/scripts.js
+++ public/scripts.js
@@ -1,39 +1,41 @@
-
 const socket = io();
 
 document.getElementById("chat-form").addEventListener("submit", (e) => {
-    e.preventDefault()
-    const messageInput = document.getElementById("message")
-    const message = messageInput.value
-    messageInput.value = ""
+  e.preventDefault();
+  const messageInput = document.getElementById("message");
+  const message = messageInput.value;
+  messageInput.value = "";
 
-    socket.emit("chatMessage", message)
-})
+  socket.emit("chatMessage", message);
+});
 
 socket.on("message", (data) => {
-    console.log(data)
-    const chatMessages = document.getElementById("chat-messages")
-    const messageElement = document.createElement("div")
-    messageElement.innerHTML = `<strong>${data.username}:</strong> ${data.message}`
-    chatMessages.appendChild(messageElement)
-})
+  const chatMessages = document.getElementById("chat-messages");
+  const messageElement = document.createElement("article");
+  messageElement.innerHTML = `<div class="bg-gray-600 text-white p-4 pt-2 rounded-xl w-4/5"><h3 class="font-semibold text-blue-300">${data.username}</h3><p>${data.message}</p></div>`;
+  chatMessages.appendChild(messageElement);
+});
 
 document.getElementById("username-form").addEventListener("submit", (e) => {
-    e.preventDefault()
-    const usernameInput = document.getElementById("username")
-    const username = usernameInput.value
+  e.preventDefault();
+  const usernameInput = document.getElementById("username");
+  const username = usernameInput.value;
 
-    socket.emit("newUser", username)
+  socket.emit("newUser", username);
 
-    Swal.fire({
-        icon: "success",
-        title: "Bienvenido al chat",
-        text: `Estas conectado como ${username}`
-    })
+  Swal.fire({
+    icon: "success",
+    title: "Welcome to the chat!",
+    text: `You are now connected as ${username}`,
+  });
 
-    document.getElementById("username-form").style.display = "none"
-    document.getElementById("chat-form").style.display = "block"
+  document.getElementById("user-login").style.display = "none";
+  document.getElementById("chat-main").style.display = "block";
+});
 
-
-
-})
+socket.on("userConnected", (username) => {
+  Swal.fire({
+    icon: "success",
+    text: `${username} is now connected`,
+  });
+});
--- server.js
+++ server.js
@@ -1,47 +1,45 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
-const { Server } = require('socket.io');
-const handlebars = require('express-handlebars');
-const Swal = require('sweetalert2');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const handlebars = require("express-handlebars");
+const Swal = require("sweetalert2");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.engine("handlebars", handlebars.engine())
+app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index.hbs");
-})
+  res.render("index.hbs");
+});
 
-const users = {}
+const users = {};
 
 io.on("connection", (socket) => {
-    console.log("Un usuario se ha conectado");
-    socket.on("newUser", (username) => {
-        users[socket.id] = username;
-        io.emit("userConnected", username)
-    })
+  console.log("Un usuario se ha conectado");
+  socket.on("newUser", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("userConnected", username);
+  });
 
+  socket.on("chatMessage", (message) => {
+    const username = users[socket.id];
+    io.emit("message", { username, message });
+  });
 
-    socket.on("chatMessage", (message) => {
-        const username = users[socket.id];
-        io.emit("message", { username, message })
-    })
-
-    socket.on("disconnect", () => {
-        const username = users[socket.id];
-        delete users[socket.id];
-        io.emit("userDisconnected", username)
-    })
-})
+  socket.on("disconnect", () => {
+    const username = users[socket.id];
+    delete users[socket.id];
+    io.emit("userDisconnected", username);
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
